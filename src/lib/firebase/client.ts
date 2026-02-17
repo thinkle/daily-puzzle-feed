1bee52111baea605dac6f67b968ce09fb0030c10
Tom Hinkle <tmhinkle@gmail.com>
2026-02-17 13:14:10 -0500

    chore(firebase): support optional measurement id env

--- src/lib/firebase/client.ts
+++ src/lib/firebase/client.ts
@@ -8,7 +8,8 @@ const firebaseConfig = {
 	projectId: import.meta.env.PUBLIC_FIREBASE_PROJECT_ID ?? '',
 	storageBucket: import.meta.env.PUBLIC_FIREBASE_STORAGE_BUCKET ?? '',
 	messagingSenderId: import.meta.env.PUBLIC_FIREBASE_MESSAGING_SENDER_ID ?? '',
-	appId: import.meta.env.PUBLIC_FIREBASE_APP_ID ?? ''
+	appId: import.meta.env.PUBLIC_FIREBASE_APP_ID ?? '',
+	measurementId: import.meta.env.PUBLIC_FIREBASE_MEASUREMENT_ID ?? undefined
 };
 
 const requiredEnvVars = [
