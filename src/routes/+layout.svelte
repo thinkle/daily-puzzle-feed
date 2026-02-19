9da8f829adcc442e028d30f08587aec66fee9b78
Tom Hinkle <tmhinkle@gmail.com>
2026-02-19 14:58:42 -0500

    +theme colors +favicon

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,12 +1,11 @@
 <script lang="ts">
-	import favicon from '$lib/assets/favicon.svg';
 	import ContainBase from '$lib/components/ContainBase.svelte';
 
 	let { children } = $props();
 </script>
 
 <svelte:head>
-	<link rel="icon" href={favicon} />
+	<link rel="icon" type="image/png" href="/daily-puzzle-feed-icon.png" />
 </svelte:head>
 
 <ContainBase />
